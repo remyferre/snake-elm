99c63b37e749311ad7db4b032dc54f31bee0e979
Rémy Ferré <remy-ferre@laposte.net>
2014-08-04 21:43:17 +0200

    Deal with wall collisions

--- Main.elm
+++ Main.elm
@@ -1,6 +1,10 @@
 import Keyboard
 import Window
 
+snakeWidth = 15
+maxWidth   = 600
+maxHeight  = 400
+
 delta : Signal Time
 delta = inSeconds <~ fps 35
 
@@ -16,9 +20,18 @@ type Game = { snake: Object }
 initGame : Game
 initGame = { snake = { x = 0, y = 0 } }
 
+stepSnake : Input -> Object -> Object
+stepSnake { up, down, delta } { x, y } =
+    { x = x + 5*(toFloat up), y = y + 5*(toFloat down) }
+
 stepGame : Input -> Game -> Game
-stepGame { up, down, delta } { snake } =
-   { snake = { x = snake.x + 5*(toFloat up), y = snake.y + 5*(toFloat down) } }
+stepGame ({ up, down, delta } as input) { snake } =
+    let hasLost = snake.x - snakeWidth / 2 < -(maxWidth  / 2) ||
+                  snake.x + snakeWidth / 2 >   maxWidth  / 2  ||
+                  snake.y - snakeWidth / 2 < -(maxHeight / 2) ||
+                  snake.y + snakeWidth / 2 >   maxHeight / 2
+    in
+      if hasLost then initGame else { snake = stepSnake input snake }
 
 gameState : Signal Game
 gameState = foldp stepGame initGame input
@@ -29,10 +42,10 @@ displayObj obj shape =
 
 display : (Int, Int) -> Game -> Element
 display (w, h) { snake } =
-    container w h middle <| collage 600 400
+    container w h middle <| collage maxWidth maxHeight
                 [
-                  filled grey (rect 600 400)
-                , displayObj snake (rect 15 15)
+                  filled grey (rect maxWidth maxHeight)
+                , displayObj snake (rect snakeWidth snakeWidth)
                 ]
 
 main = lift2 display Window.dimensions gameState
